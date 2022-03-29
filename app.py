fd8a3b879fd6c2e8e0cc3ab74c34569f0227dd0c
michaelhildner <michael.hildner@gmail.com>
2022-03-29 13:45:10 -0500

    initial commit for heroku

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, Request, request, send_from_directory
+from flask_cors import CORS, cross_origin
+
+
+app = Flask(__name__, static_folder='my-app/build', static_url_path='')
+CORS(app)
+@app.route('/api', methods=['GET'])
+@cross_origin()
+def index():
+    args = request.args.to_dict(flat=False)
+    firstName = ''
+    message = 'User Not Found'
+    if 'name' in args:
+        firstName = request.args.get('name')
+    if firstName.lower() == 'michael' :
+        message = 'hildner'
+    elif firstName.lower() == 'ta':
+        message = 'please give me a 100'
+    else:
+        message = 'User Not Found'
+
+    return {
+        "lastname" : message
+    }
+
+@app.route('/')
+@cross_origin()
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == '__main__':
+    app.run()
