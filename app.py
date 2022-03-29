556f7b1ebb69f6673587dd5e998e7a5db073195c
Michael Hildner <61170537+michaelhildner@users.noreply.github.com>
2022-03-29 16:57:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, Request, request, send_from_directory
-from flask_cors import CORS, cross_origin
+#from flask_cors import CORS, cross_origin
 
 #static folder used to route to my-app
 app = Flask(__name__, static_folder='my-app/build', static_url_path='')
-CORS(app)
+#CORS(app)
 @app.route('/api', methods=['GET'])
-@cross_origin()
+#@cross_origin()
 def index():
     args = request.args.to_dict(flat=False)
     firstName = ''
@@ -24,7 +24,7 @@ def index():
     }
 
 @app.route('/')
-@cross_origin()
+#@cross_origin()
 def serve():
     return send_from_directory(app.static_folder, 'index.html')
 
